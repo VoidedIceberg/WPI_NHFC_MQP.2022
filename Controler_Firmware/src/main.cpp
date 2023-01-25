8b09bb703503db523fe22017903302485770acee
Troy Mullenberg <tmullenberg@hotmail.com>
2023-01-25 01:58:11 -0500

    Integrated the I2C load cell and routine into main firmware
    As long as it has a different address on the bus it should be fine. Found out that I will need to use an I2C multiplexer for the encoders because they have fixed addresses and there is only one I2C bus not in use.

--- Controler_Firmware/src/main.cpp
+++ Controler_Firmware/src/main.cpp
@@ -3,66 +3,59 @@
 #include <main.h>
 #include <message_handler.h>
 #include <utils.h>
-// #include <hardwareTesting.h>
-#include <TorqueControl.h>
+#include <hardwareTesting.h>
 
-// BLDCMotor ROT_MOTOR = BLDCMotor(MOTOR_POLES, MOTOR_RESISTANCE);
-// BLDCDriver3PWM ROT_DRIVER = BLDCDriver3PWM(PC0, PC1, PC2, PC13); // M1 Port
-// MagneticSensorI2C ROT_ENCODER = MagneticSensorI2C(AS5600_I2C);
+BLDCMotor ROT_MOTOR = BLDCMotor(MOTOR_POLES, MOTOR_RESISTANCE);
+BLDCDriver3PWM ROT_DRIVER = BLDCDriver3PWM(PC0, PC1, PC2, PC13); // M1 Port
+MagneticSensorI2C ROT_ENCODER = MagneticSensorI2C(AS5600_I2C);
 
-// BLDCMotor LIN_MOTOR = BLDCMotor(MOTOR_POLES, MOTOR_RESISTANCE);
-// BLDCDriver3PWM LIN_DRIVER = BLDCDriver3PWM(PA0, PA1, PA2, PC14); // M2 Port
-// MagneticSensorI2C LIN_ENCODER = MagneticSensorI2C(AS5600_I2C);
+BLDCMotor LIN_MOTOR = BLDCMotor(MOTOR_POLES, MOTOR_RESISTANCE);
+BLDCDriver3PWM LIN_DRIVER = BLDCDriver3PWM(PA0, PA1, PA2, PC14); // M2 Port
+MagneticSensorI2C LIN_ENCODER = MagneticSensorI2C(AS5600_I2C);
 
 enum STATE state = IDLE;
 
 void setup()
 {
-  // // put your setup code here, to run once:
+  // put your setup code here, to run once:
 
-  // Serial.begin(9600); // initialize serial communication on the CDC USB port
-  // initI2C();
-  // initMotor(ROT_MOTOR, ROT_DRIVER, ROT_ENCODER);
-  // initMotor(LIN_MOTOR, LIN_DRIVER, LIN_ENCODER);
+  Serial.begin(9600); // initialize serial communication on the CDC USB port
+  initI2C();
+  initMotor(ROT_MOTOR, ROT_DRIVER, ROT_ENCODER);
+  initMotor(LIN_MOTOR, LIN_DRIVER, LIN_ENCODER);
 
-  // LIN_MOTOR.target = 0;
-  // ROT_MOTOR.target = 0;
-
-  // hTsetup();
-  tCsetup();
-
+  LIN_MOTOR.target = 0;
+  ROT_MOTOR.target = 0;
 }
 
 void loop()
 {
-  // hTloop();
-  tCloop();
-  
-  // switch (state)
-  // {
-  // case IDLE:
-  //   state = READING;
-  //   break;
-  // case READING:
-  //   if (Serial.available() > 0)
-  //   {
-  //     handelMessage(Serial, ROT_MOTOR, LIN_MOTOR);
-  //   }
-  //   state = SENDING;
-  //   break;
-  // case SENDING:
-  //   sendMovement(ROT_ENCODER, LIN_ENCODER);
-  //   state = READING;
-  //   break;
-  // default:
-  //   state = IDLE;
-  //   break;
-  // }
+  switch (state)
+  {
+  case IDLE:
+    state = READING;
+    break;
+  case READING:
+    if (Serial.available() > 0)
+    {
+      handelMessage(Serial, ROT_MOTOR, LIN_MOTOR);
+    }
+    state = SENDING;
+    break;
+  case SENDING:
+    // sendMovement(ROT_ENCODER, LIN_ENCODER);
+    state = READING;
+    break;
+    
+  default:
+    state = IDLE;
+    break;
+  }
 
-  // // Makes sure the motors run on every loop
-  // LIN_MOTOR.loopFOC();
-  // ROT_MOTOR.loopFOC();
-  // blink(1, 0);
+  // Makes sure the motors run on every loop
+  LIN_MOTOR.loopFOC();
+  ROT_MOTOR.loopFOC();
+  blink(1, 0);
 }
 
 void initMotor(BLDCMotor motor, BLDCDriver3PWM driver, MagneticSensorI2C encoder)
